d678e1f632c34c202ce27b55004b9898171e0310
Nguyen Thanh Binh <binh15122003@gmail.com>
2024-11-16 16:59:08 +0000

    Feat/auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,34 +1,51 @@
-import type { Metadata } from "next";
-import localFont from "next/font/local";
-import "./globals.css";
+import type { Metadata } from 'next';
+import localFont from 'next/font/local';
+import { Toaster } from '@/components/ui/sonner';
+import NextTopLoader from 'nextjs-toploader';
+import { auth } from '@/auth';
+import Providers from '@/components/layout/providers';
+
+import './globals.css';
+import { NextIntlClientProvider } from 'next-intl';
+import { getMessages } from 'next-intl/server';
 
 const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
+  src: './fonts/GeistVF.woff',
+  variable: '--font-geist-sans',
+  weight: '100 900'
 });
 const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+  src: './fonts/GeistMonoVF.woff',
+  variable: '--font-geist-mono',
+  weight: '100 900'
 });
 
 export const metadata: Metadata = {
-  title: "App",
-  description: "app",
+  title: 'App',
+  description: 'app'
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
+  params: { locale }
 }: Readonly<{
   children: React.ReactNode;
+  params: { locale: string };
 }>) {
+  const session = await auth();
+  const messages = await getMessages();
   return (
-    <html lang="en">
+    <html lang={locale} suppressHydrationWarning={true}>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <NextIntlClientProvider messages={messages}>
+          <NextTopLoader showSpinner={false} />
+          <Providers session={session}>
+            <Toaster />
+            {children}
+          </Providers>
+        </NextIntlClientProvider>
       </body>
     </html>
   );
