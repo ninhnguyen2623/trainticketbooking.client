13b9fb4f6b60232e7e4fa47e740b6aa5f5f37f55
nototo15 <69106519+notobo15@users.noreply.github.com>
2024-11-26 18:22:07 +0700

    added: settings page

---
+++ app/[locale]/(home)/settings/appearance/page.tsx
@@ -0,0 +1,14 @@
+import { AppearanceForm } from "./AppearanceForm";
+import ContentSection from "../components/ContentSection";
+
+export default function SettingsAppearance() {
+  return (
+    <ContentSection
+      title="Appearance"
+      desc="Customize the appearance of the app. Automatically switch between day
+          and night themes."
+    >
+      <AppearanceForm />
+    </ContentSection>
+  );
+}
