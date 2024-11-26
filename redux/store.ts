7c228814c0318e3fa20ef270befa978ed94638cb
nototo15 <69106519+notobo15@users.noreply.github.com>
2024-11-26 18:15:35 +0700

    modified: config store redux

--- redux/store.ts
+++ redux/store.ts
@@ -1,18 +1,17 @@
 import { trainApi } from "@/services/trainApi";
 import { configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from "@reduxjs/toolkit/query";
 
-export const makeStore = () => {
-  return configureStore({
-    reducer: {
-      [trainApi.reducerPath]: trainApi.reducer
-    },
-    middleware: (getDefaultMiddleware) =>
-      getDefaultMiddleware().concat(trainApi.middleware)
-  });
-};
-
-// Infer the type of makeStore
-export type AppStore = ReturnType<typeof makeStore>;
+export const store = configureStore({
+  reducer: {
+    [trainApi.reducerPath]: trainApi.reducer
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(trainApi.middleware)
+});
+setupListeners(store.dispatch);
+// Infer the type of store
+export type AppStore = ReturnType<typeof store.getState>;
 // Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<AppStore["getState"]>;
-export type AppDispatch = AppStore["dispatch"];
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
