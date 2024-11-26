e61deb0ed0e6c735091b1c87cd18f85876f745b0
nototo15 <69106519+notobo15@users.noreply.github.com>
2024-11-26 14:30:39 +0700

    chore: rename auth auth ocnfig

---
+++ auth/auth.config.ts
@@ -0,0 +1,53 @@
+import { AuthOptions } from "next-auth";
+import CredentialProvider from "next-auth/providers/credentials";
+import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+import FacebookProvider from "next-auth/providers/facebook";
+
+const authConfig: AuthOptions = {
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID ?? "",
+      clientSecret: process.env.GITHUB_SECRET ?? ""
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID ?? "",
+      clientSecret: process.env.GOOGLE_SECRET ?? ""
+    }),
+    FacebookProvider({
+      clientId: process.env.FACEBOOK_ID ?? "",
+      clientSecret: process.env.FACEBOOK_SECRET ?? ""
+    }),
+    CredentialProvider({
+      credentials: {
+        email: {
+          type: "email"
+        },
+        password: {
+          type: "password"
+        }
+      },
+      async authorize(credentials, req) {
+        const user = {
+          id: "1",
+          name: "John",
+          email: credentials?.email as string
+        };
+        if (user) {
+          // Any object returned will be saved in `user` property of the JWT
+          return user;
+        } else {
+          // If you return null then an error will be displayed advising the user to check their details.
+          return null;
+
+          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+        }
+      }
+    })
+  ],
+  pages: {
+    signIn: "/en/sign-in" //sigin page
+  }
+};
+
+export default authConfig;
