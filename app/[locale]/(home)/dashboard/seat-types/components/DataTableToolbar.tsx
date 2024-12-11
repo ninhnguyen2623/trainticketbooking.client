cea911f08e66ec79d9e5d64a449960317122049e
ninhnguyen2623 <ninh1160@gmail.com>
2024-12-11 20:22:06 +0700

    update seat-types

--- app/[locale]/(home)/dashboard/seat-types/components/DataTableToolbar.tsx
+++ app/[locale]/(home)/dashboard/seat-types/components/DataTableToolbar.tsx
@@ -18,10 +18,10 @@ export function DataTableToolbar<TData>({
       <div className="flex flex-1 flex-col-reverse items-start gap-y-2 sm:flex-row sm:items-center sm:space-x-2">
         <div className="flex flex-row gap-x-2">
           <Input
-            placeholder="Filter name..."
-            value={(table.getColumn("name")?.getFilterValue() as string) ?? ""}
+            placeholder="Filter type..."
+            value={(table.getColumn("type")?.getFilterValue() as string) ?? ""}
             onChange={(event) =>
-              table.getColumn("name")?.setFilterValue(event.target.value)
+              table.getColumn("type")?.setFilterValue(event.target.value)
             }
             className="h-8 w-[150px] lg:w-[250px]"
           />
